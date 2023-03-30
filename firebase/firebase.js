83eedfd600669d3577b7f1f61d997c34610a9eb0
Sanwal Hussain <106494523+sanwalhussain@users.noreply.github.com>
2023-03-30 03:20:27 +0300

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -7,9 +7,14 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    // Paste in firestore configuration here.
-    projectId: 'abc'
-}
+    apiKey: "AIzaSyC1qkh80bgByJ830oLGoheqKhS1XRYcdUE",
+    authDomain: "blogpost-37ec4.firebaseapp.com",
+    projectId: "blogpost-37ec4",
+    storageBucket: "blogpost-37ec4.appspot.com",
+    messagingSenderId: "955586200752",
+    appId: "1:955586200752:web:18db4bed7b9705f6d96775",
+    measurementId: "G-4J6VKYT2CT"
+  };
 
 var app;
 
